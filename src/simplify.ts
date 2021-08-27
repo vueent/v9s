8e923d1a52a2cb051a2b11cc89b6dbee7ddec620
Alexey Nosov <nayretech@gmail.com>
2021-08-27 11:49:00 +0700

    3-v9s Update comments

--- src/simplify.ts
+++ src/simplify.ts
@@ -3,6 +3,8 @@ import Validator from './validator';
 /**
  * Returns an error message or `true`.
  *
+ * This function prevents to use a `true` value as an error.
+ *
  * @param v - {@link Validator} instance
  * @returns - error message or `true`
  */
